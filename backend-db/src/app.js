ac9c1c1fefa5e2291baf4d5f6e5aeffd7d0274a3
ARPITPRAJAPATI <arpitprajapati2005@gmail.com>
2026-07-15 22:20:44 +0530

    feat: set up server with Express and MongoDB connection

--- backend-db/src/app.js
+++ backend-db/src/app.js
@@ -1,5 +1,54 @@
 const express = require('express')
+const noteModel = require("./models/notes.model");
+const { get } = require('mongoose');
+
 
 const app = express();
 
-module.exports = app;
+app.use(express.json());
+
+app.post("/notes" , async (req,res) => {
+    const data = req.body
+    await noteModel.create({
+        title: data.title,
+        description: data.description
+    })
+    res.status(201).json({
+        message: "note created"
+    })
+})
+app.get("/notes/:id", async(req,res) => {
+    const id = req.params.id
+    const notes = await noteModel.find({
+        _id:id
+    });
+
+    res.status(200).json({
+        message: "notes get",
+        notes: notes
+    })
+})
+app.delete("/notes/:id", async(req,res) => {
+    const id = req.params.id
+    const notes = await noteModel.findOneAndDelete({
+        _id:id
+    });
+
+    res.status(200).json({
+        message: "note deleted",
+    })
+})
+app.patch("/notes/:id", async (req,res)=>{
+    const id = req.params.id
+    const description = req.body.description
+    const updatedNote = await noteModel.updateOne({ _id:id} , {description:description}   
+
+    );
+    res.status(200).json({
+            message: "Note updated successfully",
+            note: updatedNote
+        });
+})
+
+
+module.exports = app;;
